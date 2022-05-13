67a2da353a2b7914dbe4697f2a6332408e8badd9
Trungdev <trungphamk53@gmail.com>
2022-05-13 08:09:57 +0700

    fix loading center and header fix when scroll

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ https://cozy-sunshine-aec5db.netlify.app/   deploy add homepage
 https://vermillion-croquembouche-2f012d.netlify.app/    reponsive homepage and add pagination
 https://splendid-cassata-4105c0.netlify.app (add them reponsive search in Result)
 https://dapper-fox-5fb435.netlify.app (master)
+https://adorable-zabaione-fbe893.netlify.app/ (style result-image)
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 import { useResultContext } from '../Context/ContextResult'
 import { GoSearch } from 'react-icons/go';
@@ -13,8 +13,28 @@ const Search = () => {
         { to: "/add", value: "Thêm" }]
     let { searchInput, inputHome, setInputHome, handleSearch } = useResultContext()
     console.log('searchinput', searchInput)
+    function handleScroll() {
+        let scroll = window.pageYOffset
+        console.log(scroll)
+        if (scroll > 150) {
+            document.querySelector(".search-header").style.position = "fixed"
+            document.querySelector(".search-header").style.boxShadow = " 0 1px 6px 0 rgba(32,33, 36,  0.28)"
+        }
+        else {
+            document.querySelector(".search-header").style.position = "static"
+            document.querySelector(".search-header").style.boxShadow = "none"
+        }
+    }
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    })
+
     return (
-        <div>
+        <div className="test">
             <div className="search-header">
                 <div className='search-header__logo'>
                     <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png" alt="logo google" />
